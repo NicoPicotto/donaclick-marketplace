84ea25c950d01b7224624420a0946e6d9f0932ae
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-06-26 21:05:52 -0300

    vercel

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,11 +1,9 @@
-import { Stack, Heading, Button } from "@chakra-ui/react";
+import { Stack, Heading } from "@chakra-ui/react";
 
 const HomeView = () => {
    return (
       <Stack>
-         <Heading>Hello world!</Heading>
-         <Button>Hola vos</Button>
-         <Button variant='outline'>Hola vos</Button>
+         <Heading>Esto está en develop</Heading>
       </Stack>
    );
 };
